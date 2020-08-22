0291b446307fcae434df0a10d224450ece570e9a
Douglas Fernandes <douglasf.filho@gmail.com>
2020-08-22 17:46:53 -0300

    refactor: repository functionality

--- src/repositories/cards.js
+++ src/repositories/cards.js
@@ -3,7 +3,7 @@ const cards = [credit, debit];
 
 const CardsRespository = {
     findByCode(cardCode) {
-        return cards.filter(card => card.code === cardCode);
+        return cards.filter(card => card.code === cardCode) || {};
     }
 };
 
