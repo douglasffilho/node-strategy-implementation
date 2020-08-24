64fd3e4d537d0ef2c5f5a643db991cf428f5e1a5
Douglas Fernandes <douglasf.filho@gmail.com>
2020-08-24 09:46:33 -0300

    add: cards repository functionality and tests

--- src/repositories/cards.js
+++ src/repositories/cards.js
@@ -2,6 +2,9 @@ const { credit, debit } = require('../domain/cards');
 const cards = [credit, debit];
 
 const CardsRespository = {
+    findAll() {
+        return cards;
+    },
     findByCode(cardCode) {
         return (cards.filter((card) => card.code === cardCode) || [{}])[0];
     }
--- test/repositories/cards.test.js
+++ test/repositories/cards.test.js
@@ -1,4 +1,5 @@
 const { cardsRepository } = require('../../src/repositories');
+const { credit, debit } = require('../../src/domain/cards');
 
 describe('CardsRepository tests', () => {
     test('should find credit card by code', () => {
@@ -7,9 +8,10 @@ describe('CardsRepository tests', () => {
         // when
         const card = cardsRepository.findByCode(cardCode);
         // then
-        const { code, type } = card;
+        const { code, type, securityCode } = card;
         expect(code).toBe(cardCode);
         expect(type).toBe('credit');
+        expect(securityCode).toBe('042');
     });
 
     test('should find debit card by code', () => {
@@ -21,5 +23,25 @@ describe('CardsRepository tests', () => {
         const { code, type } = card;
         expect(code).toBe(cardCode);
         expect(type).toBe('debit');
+    });
+
+    test('should not find card by unknown code', () => {
+        // given
+        const cardCode = '**** **** **** 0021';
+        // when
+        const card = cardsRepository.findByCode(cardCode);
+        // then
+        expect(card).toBeUndefined();
+    });
+
+    test('should find all cards', () => {
+        // given
+        const cards = [credit, debit];
+        const cardsSerialized = JSON.stringify(cards, null, 2);
+        // when
+        const foundCards = cardsRepository.findAll();
+        const foundCardsSerialized = JSON.stringify(foundCards, null, 2);
+        // then
+        expect(foundCardsSerialized).toBe(cardsSerialized);
     });
 });
